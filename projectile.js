8a0d6d5d18f9b56bd8245c3783d2ad1ec2de87ff
Miguel Rentes <miguel.rentes@gmail.com>
2015-01-29 15:35:54 +0000

    Added a ball when the user clicks on the canvas.

--- projectile.js
+++ projectile.js
@@ -1,3 +1,50 @@
 /**
  * Created by miguel on 27/01/15.
  */
+var posX; /* mouse position co-ordinate X */
+var posY; /* mouse position co-ordinate Y */
+
+/* canvas details */
+var width = 800;
+var height = 600;
+var canvas;
+var ctx;
+
+/* projectile detail */
+var projectile;
+
+function initializeProjectile(event) {
+    posX = event.clientX;
+    posY = event.clientY;
+    projectile = {
+        position: {x: posX, y: posY},
+        velocity: {x: 10, y: 0},
+        mass: 0.1,
+        radius: 15
+    };
+    console.log('projectile position: '+ projectile.position.x + ', ' + projectile.position.y);
+}
+
+function drawBall() {
+    console.log('no drawBall');
+    ctx.clearRect(0, 0, width, height);
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(projectile.position.x, projectile.position.y, 10, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'grey';
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+}
+
+function initializeCanvas() {
+    canvas = document.getElementById('projectile');
+    ctx = canvas.getContext('2d');
+    drawBall();
+}
+
+function drawProjectile(event) {
+    initializeProjectile(event);
+    initializeCanvas();
+}
